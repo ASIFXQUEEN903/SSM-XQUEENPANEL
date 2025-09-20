eddbcb3bd0c8d7e60a94036c8f276cd710b6cd90
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-09-20 12:18:16 +0530

    XQUEEN

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os, requests, logging
 logger = logging.getLogger(__name__)
 
 TEMPORA_API_KEY = os.getenv("TEMPORA_API_KEY", "TEMPORA_API_KEY_HERE")
-TEMPORA_BASE = "https://api.undefined/stubs/handler_api.php"
+TEMPORA_BASE = "https://api.temporasms.com/stubs/handler_api.php"
 
 def call_tempora_api(action, extra_params=None):
     params = {"action": action, "api_key": TEMPORA_API_KEY}
