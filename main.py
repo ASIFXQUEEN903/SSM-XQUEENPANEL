ffcb8d6302a1caef12d69fe01faadc5107c2e45f
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-11-01 06:09:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -199,7 +199,7 @@ def callback(call):
         return
 
         if data == "choose_usa":
-        kb = InlineKeyboardMarkup(row_width=2)
+            kb = InlineKeyboardMarkup(row_width=2)
         kb.add(
             InlineKeyboardButton(" Telegram — ₹50", callback_data="buy_telegram"),
             InlineKeyboardButton(" WhatsApp — ₹45", callback_data="buy_whatsapp")
